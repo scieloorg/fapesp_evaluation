80523b3b2348f9f449224aaf88ce1724aa780ee4
ednilson <edy.gesseff@gmail.com>
2018-06-06 14:53:03 -0300

    Adicao de ProxyFix

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_script import Manager
 from flask_mongoengine import MongoEngine
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,3 +11,5 @@ manager = Manager(app)
 
 from app.models import models, forms
 from app.controllers import default
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
